26f7c7680d4e056d819ba3f02549dbdb1dbfcad6
Gonzalo Lafuente <lafuentegonzalo1@gmail.com>
2026-09-20 18:41:50 -0300

    Mas SQL

--- Clases-Practica/Guias/Guia-3/Guia-3.sql
+++ Clases-Practica/Guias/Guia-3/Guia-3.sql
@@ -32,10 +32,34 @@ from Playlist p join PlaylistTrack pt on p.PlaylistId = pt.PlaylistId
 join Track t on pt.TrackId = t.TrackId 
 group by p.name
 
-select e.EmployeeId
+select distinct e.EmployeeId
 from Employee e join Customer c on e.EmployeeId = c.SupportRepId
 join Invoice i on i.CustomerId = c.CustomerId
 join InvoiceLine il on i.InvoiceId = il.InvoiceId 
-where DATEDIFF(YY,e.BirthDate, GETDATE()) > 25
-group by e.EmployeeId, il.InvoiceLineId
+where DATEDIFF(YEAR,e.BirthDate, GETDATE()) > 25
+group by e.EmployeeId, i.InvoiceId
 having COUNT(il.InvoiceLineId) > 10
+
+select distinct e.EmployeeId
+from Employee e
+JOIN Customer c ON e.EmployeeId = c.SupportRepId
+JOIN Invoice i ON c.CustomerId = i.CustomerId
+GROUP BY e.EmployeeId, c.CustomerId
+HAVING COUNT(i.InvoiceId) > 10;
+
+select e1.FirstName, e1.LastName, e2.FirstName as FirstName_boss, e2.LastName as LastName_boss
+from Employee e1 join Employee e2 on e1.ReportsTo = e2.EmployeeId
+
+select e1.FirstName, e1.LastName, e2.FirstName as FirstName_boss, e2.LastName as LastName_boss
+from Employee e1 left join Employee e2 on e1.ReportsTo = e2.EmployeeId
+
+
+select sub.CustomerId, AVG(sub.sum_track) as track_avg
+from (
+    select c.CustomerId, COUNT(il.InvoiceId) as sum_track
+    from customer c join Invoice i on i.CustomerId = c.CustomerId
+    join InvoiceLine il on i.InvoiceId = il.InvoiceId
+    group by c.CustomerId, i.InvoiceId
+) as sub
+group by CustomerId 
+order by CustomerId
